081e29c3628ed7e74924544d32e1ed1aab85e807
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-05-03 09:27:58 -0700

    chore: Refactor onReady function to improve application command refreshing

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -6,30 +6,30 @@ import Config from '../config';
 
 export function onReady(client: Client) {
   client.once('ready', async () => {
-    if (client.user) {
-      logger.info(`Ready! Logged in as ${client.user.tag}`);
-      client.user.setPresence({
-        activities: [{ name: 'The Universe', type: ActivityType.Watching }],
-        status: 'online',
-      });
-      await logToChannel(client, `Logged in as ${client.user.tag}`);
+    logger.info(`Ready! Logged in as ${client.user?.tag}`);
 
-      const rest = new REST({ version: '10' }).setToken(Config.DISCORD_TOKEN);
+    client.user?.setPresence({
+      activities: [{ name: 'The Universe', type: ActivityType.Watching }],
+      status: 'online',
+    });
 
-      try {
-        logger.info('Started refreshing application (/) commands.');
-
-        const body = Array.from(commands.values()).map(command => command.data.toJSON());
-        
-        await rest.put(
-          Routes.applicationCommands(client.user.id),
-          { body },
-        );
-
-        logger.info('Successfully reloaded application (/) commands.');
-      } catch (error) {
-        logger.error('Failed to reload application (/) commands:', error);
-      }
-    }
+    await logToChannel(client, `Logged in as ${client.user?.tag}`);
+    await registerCommands(client);
   });
+}
+
+async function registerCommands(client: Client) {
+  if (!client.user) return;
+
+  const rest = new REST().setToken(Config.DISCORD_TOKEN);
+  try {
+    logger.info('Started refreshing application (/) commands.');
+    const body = Array.from(commands.values()).map((command) => command.data.toJSON());
+
+    await rest.put(Routes.applicationCommands(client.user.id), { body });
+
+    logger.info('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    logger.error('Failed to reload application (/) commands:', error);
+  }
 }
